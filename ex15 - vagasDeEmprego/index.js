279d09eda4f4ccbcdaea7c258edddd0afd3dd727
renatodinizz <154015377+renatodinizz@users.noreply.github.com>
2026-03-15 17:26:25 -0300

    exercicio 15

--- ex15 - vagasDeEmprego/index.js
+++ ex15 - vagasDeEmprego/index.js
@@ -1,5 +1,6 @@
 let menu = ''
 const vagas = []
+
 function exibirMenu() {
     return prompt(
         "Sistema de vagas de emprego\n" +
@@ -11,19 +12,15 @@ function exibirMenu() {
         "6. Sair\n"
     )
 }
-function listarVagas(){
-    let mensagem = ""
+function listarVagas() {
+    const vagasEmTexto = vagas.reduce((textoFinal, vaga, indice) => {
+        textoFinal += indice + ". "
+        textoFinal += vaga.nome
+        textoFinal += " (" + vaga.candidatos.length + " candidatos)\n"
+        return textoFinal
+    }, "")
 
-    vagas.forEach(function(vaga, indice){
-        mensagem += `
-    Candidatos inscritos:  
-
-    Índice: ${vaga[indice].indice}
-    Nome: ${vaga[indice].nome}
-    `
-    })
-
-alert(mensagem)
+    alert(vagasEmTexto)
 }
 function criarNovaVaga(){
     let nomeDaVaga = prompt(`Qual o nome da vaga?`)
@@ -48,45 +45,76 @@ function criarNovaVaga(){
         vaga.data = dataLimite
         vaga.candidatos = candidatos
         vagas.push(vaga)
+        alert(`Vaga criada!`)
     }
 }
 function visualizarUmaVaga(){
     let id = Number(prompt(`Qual o índice da vaga?`))
+    if (id >= vagas.length || id < 0){
+        alert(`Índice inválido!`)
+        return
+    }
     alert(
-        `
-        Informações da vaga:
-    
-        Índice: ${vagas.indice}
-        Nome: ${vagas[id].nome}
-        Descrição : ${vagas[id].descricao}
-        Data limite : ${vagas[id].data}
-        Quantidade de candidatos: ${vagas[id].candidatos.length}
-        Candidatos:
-        ${vagas[id].candidatos.join('\n')}
-        `)
+`
+Informações da vaga:
+
+Índice: ${vagas[id].indice}
+Nome: ${vagas[id].nome}
+Descrição : ${vagas[id].descricao}
+Data limite : ${vagas[id].data}
+Quantidade de candidatos: ${vagas[id].candidatos.length}
+Candidatos:
+${vagas[id].candidatos.join('\n')}
+`)
 }
 function cadastrarCandidato (){
     let nome = prompt(`Qual o nome do candidato?`)
     let indice = Number(prompt(`Qual o índice da vaga?`))
+    if (indice >= vagas.length || indice < 0){
+        alert(`Índice inválido!`)
+        return
+    }
     let confirmacao = confirm(
         `
         Informações da vaga:
-    
+        
         Nome: ${vagas[indice].nome}
         Descrição : ${vagas[indice].descricao}
         Data limite : ${vagas[indice].data}
-    
+        
         Deseja cadastrar ${nome} nessa vaga?
         `)
     if (confirmacao){
         vagas[indice].candidatos.push(nome)
     }
 }
+function excluirVaga(){
+    let indice = Number(prompt(`Qual o índice da vaga?`))
+    if (indice >= vagas.length || indice < 0){
+        alert(`Índice inválido!`)
+        return
+    }
+    let confirmacao = confirm(
+        `
+        Informações da vaga:
+        
+        Nome: ${vagas[indice].nome}
+        Descrição : ${vagas[indice].descricao}
+        Data limite : ${vagas[indice].data}
+        
+        Deseja excluir essa vaga?
+        `)
+    if (confirmacao){
+        vagas.splice(indice, 1)
+        alert(`Vaga excluída com sucesso!`)
+    }      
+}
 
 while(menu !='6'){
     menu = exibirMenu()
     switch(menu){
-        case '1':            
+        case '1': 
+            listarVagas()           
             break
         case '2':  
             criarNovaVaga()          
@@ -98,6 +126,7 @@ while(menu !='6'){
             cadastrarCandidato()         
             break
         case '5':
+            excluirVaga()
             break
         case '6':
             alert(`Encerrando...`)
